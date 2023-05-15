f1f190d2902d7b86fe4ece1aaba28feceabba1f3
luizgpolido <luiz_polido@outlook.com>
2023-05-14 22:22:15 -0300

    test

--- Estudos/src/main/java/org/example/Aula02TiposPrimitivos.java
+++ Estudos/src/main/java/org/example/Aula02TiposPrimitivos.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
-        System.out.println("Teste");
+        // int, double, float, char, byte, short, long, boolean
+
     }
 }
